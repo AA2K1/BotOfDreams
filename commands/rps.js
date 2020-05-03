9f019b778b2ab1a47ebab263954e831797c157fc
AA2K1 <AA2K1@users.noreply.github.com>
2020-05-02 22:48:35 -0400

    rps fix 4

--- commands/rps.js
+++ commands/rps.js
@@ -11,34 +11,24 @@ module.exports = {
         const pickCollect = new MessageCollector(message.channel, m => m.author.id == message.author.id);
         pickCollect.on('collect', message => {
             let humanPick = message.content;
+            if(humanPick !== 'rock' || humanPick !== 'paper' || humanPick !== 'scissors') return;
             let botPick = chooseArr[Math.floor(Math.random() * chooseArr.length)];
             const result = getResult(humanPick, botPick);
 
+            const embed = new MessageEmbed() 
+                .setTitle(`${message.author.username} vs ${client.user.username}`)
+                .addField(`Result: `, `${humanPick} to ${botPick}`)
+                .setDescription(result)
+                .setTimestamp()
+                .setFooter(client.user.username, client.user.displayAvatarURL)
+
             function getResult(humanChosenOption, me) {
                 if((me === "rock" && humanChosenOption === "paper") || (me === "paper" && humanChosenOption === "scissors") || (me === "scissors" && humanChosenOption === "rock")) {
-                    const embed = new MessageEmbed() 
-                        .setTitle(`${message.author.username} vs ${client.user.username}`)
-                        .addField(`Result: `, `${humanChosenOption} to ${me}`)
-                        .setDescription(`Looks like you win, ${message.author.username}. Not bad.`)
-                        .setTimestamp()
-                        .setFooter(client.user.username, client.user.displayAvatarURL)
-                    return message.channel.send(embed);
+                    return `Looks like you win, ${message.author.username}. Not bad.`;
                 } else if(me === humanChosenOption) {
-                    const embed1 = new MessageEmbed() 
-                        .setTitle(`${message.author.username} vs ${client.user.username}`)
-                        .addField(`Result: `, `${humanChosenOption} to ${me}`)
-                        .setDescription(`Looks like it's a tie.`)
-                        .setTimestamp()
-                        .setFooter(client.user.username, client.user.displayAvatarURL)
-                    return message.channel.send(embed1);
+                    return `Looks like it's a tie.`;
                 } else {
-                    const embed2 = new MessageEmbed() 
-                        .setTitle(`${message.author.username} vs ${client.user.username}`)
-                        .addField(`Result: `, `${humanChosenOption} to ${me}`)
-                        .setDescription(`Looks like you lose, ${message.author.username}. You suck.`)
-                        .setTimestamp()
-                        .setFooter(client.user.username, client.user.displayAvatarURL)
-                    return message.channel.send(embed2);
+                    return `Looks like you lose, ${message.author.username}. You suck.`;
                 }
             }
         })
