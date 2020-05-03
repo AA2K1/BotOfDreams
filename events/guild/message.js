7118539c80f9d7453714eb53da99fc08036007fa
AA2K1 <AA2K1@users.noreply.github.com>
2020-05-03 19:13:25 -0400

    message.js changes

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,15 +1,11 @@
 module.exports = async (message) => {
     const prefix = '~';
-    const messageArray = message.content.split(" ");
-    const args = messageArray.slice(1);//message.content.slice(1).trim().split(/ +/g);
-    const cmd = messageArray[0].toLowerCase();   //args.shift().toLowerCase();
+    if(message.author.bot || message.channel.type === "dm") return;
 
-    if (message.author.bot) return;
-    if (!message.guild) return;
-    if (!message.content.startsWith(prefix)) return;
-    if (!message.member) message.member = await message.guild.fetchMember(message);
-    if(cmd.length === 0) return;
+    let args = message.content.slice(prefix.length).trim().split(/ +/g);
+    let cmd = args.shift().toLowerCase();
 
-    let commandfile = client.commands.get(cmd) || client.comands.get(client.aliases.get(cmd));
-    if(commandfile) commandfile.run(message, args, client);
+    if(!message.content.startsWith(prefix)) return;
+    let commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd))
+    if(commandfile) commandfile.run(bot, message, args)
 }
