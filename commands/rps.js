20d540c175eb902206a459d94a5ea443b99f5cb7
AA2K1 <AA2K1@users.noreply.github.com>
2020-05-02 23:09:26 -0400

    big boy rps fix: now uses reactions

--- commands/rps.js
+++ commands/rps.js
@@ -7,33 +7,58 @@ module.exports = {
     category: "fun",
     description: "Plays a nice friendly game of rock paper scissors.",
     run: async (message, args, client) => {
-        message.reply("Type rock, paper, or scissors into chat.");
-        const pickCollect = new MessageCollector(message.channel, m => m.author.id == message.author.id, {time: 10000});
-        pickCollect.on('collect', message => {
-            let humanPick = message.content;
-            //if(humanPick !== 'rock' || humanPick !== 'paper' || humanPick !== 'scissors') return;
-            let botPick = chooseArr[Math.floor(Math.random() * chooseArr.length)];
-            const result = getResult(humanPick, botPick);
+        const embed = new RichEmbed()
+            .setColor("#ffffff")
+            .setFooter(message.guild.me.displayName, client.user.displayAvatarURL)
+            .setDescription("Add a reaction to one of these emojis to play the game!")
+            .setTimestamp();
 
-            function getResult(humanChosenOption, me) {
-                if((me === "rock" && humanChosenOption === "paper") || (me === "paper" && humanChosenOption === "scissors") || (me === "scissors" && humanChosenOption === "rock")) {
-                    return `Looks like you win, ${message.author.username}. Not bad.`;
-                } else if(me === humanChosenOption) {
-                    return `Looks like it's a tie.`;
-                } else {
-                    return `Looks like you lose, ${message.author.username}. You suck.`;
-                }
+        const m = await message.channel.send(embed);
+        // Wait for a reaction to be added
+        const reacted = await promptMessage(m, message.author, 30, chooseArr);
+
+        // Get a random emoji from the array
+        const botChoice = chooseArr[Math.floor(Math.random() * chooseArr.length)];
+
+        // Check if it's a win/tie/loss
+        const result = await getResult(reacted, botChoice);
+        // Clear the reactions
+        await m.clearReactions();
+
+        embed
+            .setDescription("")
+            .addField(result, `${reacted} vs ${botChoice}`);
+
+        m.edit(embed);
+
+        function getResult(me, clientChosen) {
+            if ((me === "🗻" && clientChosen === "✂") ||
+                (me === "📰" && clientChosen === "🗻") ||
+                (me === "✂" && clientChosen === "📰")) {
+                    return "You won!";
+            } else if (me === clientChosen) {
+                return "It's a tie!";
+            } else {
+                return "You lost!";
             }
-
-            const embed = new MessageEmbed() 
-                .setColor(0x96fac5)
-                .setTitle(`${message.author.username} vs ${client.user.username}`)
-                .addField(`Result: `, `${humanPick} to ${botPick}`)
-                .setDescription(result)
-                .setTimestamp()
-                .setFooter(client.user.username, client.user.displayAvatarURL)
-            message.channel.send(embed);
-        })
+        }
         
+    },
+
+    promptMessage: async function (message, author, time, validReactions) {
+        // We put in the time as seconds, with this it's being transfered to MS
+        time *= 1000;
+
+        // For every emoji in the function parameters, react in the good order.
+        for (const reaction of validReactions) await message.react(reaction);
+
+        // Only allow reactions from the author, 
+        // and the emoji must be in the array we provided.
+        const filter = (reaction, user) => validReactions.includes(reaction.emoji.name) && user.id === author.id;
+
+        // And ofcourse, await the reactions
+        return message
+            .awaitReactions(filter, { max: 1, time: time})
+            .then(collected => collected.first() && collected.first().emoji.name);
     }
 }
