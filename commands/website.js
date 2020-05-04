d805f8d6199b16ea902a19d8365a1171e30373f6
AA2K1 <AA2K1@users.noreply.github.com>
2020-05-03 21:06:28 -0400

    changes

--- commands/website.js
+++ commands/website.js
@@ -1,19 +1,19 @@
-const { MessageEmbed } = require('discord.js');
-module.exports = {
-    config: {
-        name: "website",
-        aliases: ["aa2k", "site"],
-        usage: `~website`,
-        category: "info",
-        description: "Returns link to my website",
-    },
-    run: async (message, args, client, prefix) => {
-        const embed1 = new MessageEmbed()
-            .setTitle('Website: ')
-            .setDescription('https://aa2k.netlify.app/')
-            .setColor(0x96fac5)
-            .setTimestamp()
-            .setFooter(client.user.username, client.user.displayAvatarURL)
-        message.channel.send(embed1)
-    }
-}
+// const { MessageEmbed } = require('discord.js');
+// module.exports = {
+//     config: {
+//         name: "website",
+//         aliases: ["aa2k", "site"],
+//         usage: `~website`,
+//         category: "info",
+//         description: "Returns link to my website",
+//     },
+//     run: async (message, args, client, prefix) => {
+//         const embed1 = new MessageEmbed()
+//             .setTitle('Website: ')
+//             .setDescription('https://aa2k.netlify.app/')
+//             .setColor(0x96fac5)
+//             .setTimestamp()
+//             .setFooter(client.user.username, client.user.displayAvatarURL)
+//         message.channel.send(embed1)
+//     }
+// }
