9e895ba6d866a17a90d4aa1df27f9a5153b8ceb3
AA2K1 <AA2K1@users.noreply.github.com>
2020-05-03 21:21:35 -0400

    changes

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ client.aliases = new Collection();
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
     const command = require(`./commands/${file}`);
-    const aliases = require(`./commands/${file}`);
+    //const aliases = require(`./commands/${file}`);
     client.commands.set(command.name, command);
-    client.aliases.set(command.aliases, aliases);
+    //client.aliases.set(command.aliases, aliases);
 }
 
 client.on("ready", () => {
     console.log(`Let us start the game, ${client.user.username}`);
-    client.user.setActivity("memes", {type: "WATCHING"});
+    client.user.setActivity("memes on /r/okbuddyretard", {type: "WATCHING"});
 });
 
 client.on('message', message => {
@@ -30,28 +30,28 @@ client.on('message', message => {
 
     let commandfile = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
     switch(cmd) {
-        case 'blip' || client.commands.get('blip').aliases:
+        case 'blip':
             client.commands.get('blip').run(message, args, client, prefix);
         break;
-        case 'help' || client.commands.get('help').aliases:
+        case 'help':
             client.commands.get('help').run(message, args, client, prefix);
         break;
-        case 'meme' || client.commands.get('meme').aliases:
+        case 'meme':
             client.commands.get('meme').run(message, args, client, prefix);
         break;
-        case 'reddit' || client.commands.get('reddit').aliases:
+        case 'reddit':
             client.commands.get('reddit').run(message, args, client, prefix);
         break;
-        case 'rps' || client.commands.get('rps').aliases:
+        case 'rps':
             client.commands.get('rps').run(message, args, client, prefix);
         break;
-        case 'say' || client.commands.get('say').aliases:
+        case 'say':
             client.commands.get('say').run(message, args, client, prefix);
         break;
-        case 'userinfo' || client.commands.get('userinfo').aliases:
+        case 'userinfo':
             client.commands.get('userinfo').run(message, args, client, prefix);
         break;
-        case 'website' || client.commands.get('website').aliases:
+        case 'website':
             client.commands.get('website').run(message, args, client, prefix);
     }
     
