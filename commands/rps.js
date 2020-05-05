cc761fd793abacecade2521fc8f11042c7e851eb
AA2K1 <AA2K1@users.noreply.github.com>
2020-05-04 23:26:11 -0400

    changes to money system in rps.js

--- commands/rps.js
+++ commands/rps.js
@@ -27,16 +27,12 @@ module.exports = {
             .setDescription(`React to this message in order to pick your choice.`)
         const m = await message.channel.send(embed)
         const reacted = await promptMessage(m, message.author, 30, chooseArr)
-        await m.delete({}, 5000)
         //await m.clearReactions();
 
         const botChoice = chooseArr[Math.floor(Math.random() * chooseArr.length)]
         let won = false;
         let moneyGained = Math.floor(Math.random() * 5) + 1;
         // const result = getWinner(reacted, botChoice);
-        if(won) {
-            coins[message.author.id].coins += moneyGained;
-        }
         embed
             .setTitle(`**You: ${reacted} vs Me: ${botChoice}**`)
             .setDescription(getWinner(reacted, botChoice));
@@ -47,7 +43,7 @@ module.exports = {
             if (botOption === humanOption) {
                 return `Looks like it's a tie, ${message.author.username}. Well played.`
             } else if ((botOption === '🔥' && humanOption === '💦') || (botOption === '💦' && humanOption === '🌱') || (botOption === '🌱' && humanOption === '🔥')) {
-                won = true;
+                coins[message.author.id].coins += moneyGained;
                 return `Looks like you won, ${message.author.username}. That was just luck. Got ${moneyGained} coins.`
             } else if ((botOption === '🔥' && humanOption === '🌱') || (botOption === '💦' && humanOption === '🔥') || (botOption === '🌱' && humanOption === '💦')) {
                 return `Looks like I win, ${message.author.username}. Easy win, you suck.`
